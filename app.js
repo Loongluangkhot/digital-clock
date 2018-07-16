64f1889693d7b811e4e1b27bdaef4bae50ac3ca9
loongluangkhot <loongluangkhot@gmail.com>
2018-07-16 12:01:25 +0800

    working prototype

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ let hourSelector = document.querySelector('#hour')
 let minuteSelector = document.querySelector('#minute')
 let secondSelector = document.querySelector('#second')
 
-// get time
-const fullDate = new Date()
-const hour = fullDate.getHours()
-const minute = fullDate.getMinutes()
-const second = fullDate.getSeconds()
+// function clock() to update time
+function clock() {
 
-// update time
-hourSelector.innerHTML = `${hour} :`
-minuteSelector.innerHTML = `${minute} :`
-secondSelector.innerHTML = `${second}`
+    // get time
+    const fullDate = new Date()
+    const hour = fullDate.getHours()
+    const minute = fullDate.getMinutes()
+    const second = fullDate.getSeconds()
 
-// function takes in number as argument, and convert number to length-2 string
-function twoChar(number) {
-    let twoCharStr = ''
-    if (number < 10) {
-        twoCharStr = `0${number}`
-    } else {
-        twoCharStr = `${number}`
+    // function takes in number as argument, and convert number to length-2 string
+    function twoChar(number) {
+        let twoCharStr = ''
+        if (number < 10) {
+            twoCharStr = `0${number}`
+        } else {
+            twoCharStr = `${number}`
+        }
+        return twoCharStr
     }
-    return twoCharStr
+
+    // update time
+    hourSelector.innerHTML = `${twoChar(hour)} :`
+    minuteSelector.innerHTML = `${twoChar(minute)} :`
+    secondSelector.innerHTML = `${twoChar(second)}`
 }
+
+// call clock() to update time every second
+setInterval(clock,100)
